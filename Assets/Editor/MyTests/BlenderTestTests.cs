ca4ab66b4ebd7eed222982fda53670406d05299c
Chris Mahoney <chrism@unity3d.com>
2016-01-04 13:08:11 -0600

    Added unit tests

---
+++ Assets/Editor/MyTests/BlenderTestTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace UnityTest
+{
+	[TestFixture]
+	[Category("Sample Tests")]
+	internal class BlenderTestTests
+	{
+		[Test]
+		[Category("Failing Tests")]
+		public void ExceptionTest()
+		{
+			throw new Exception("Exception throwing test");
+		}
+
+		[Test]
+		[Category("Successful Tests")]
+		public void SuccessTest()
+		{
+			Assert.IsTrue(true);
+		}
+
+	}
+
+}
